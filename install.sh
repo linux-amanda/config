7ebb3965c9d01d0d206cc47a5cc9687dff5b0859
Sultan <138652076+sulfar0@users.noreply.github.com>
2026-07-23 18:33:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -309,7 +309,7 @@ sleep 2
 # --- 6. CONFIG WALLPAPER PLASMA ---
 log_info "Memasang Wallpaper default"
 rm /mnt/usr/share/wallpapers/Next/contents/images/*
-cp -r config/* /mnt 
+cp -r amanda/* /mnt 
 arch-chroot /mnt sed -i '/favoritesPortedToKAstats=true/a icon=\/usr\/share\/pixmaps\/amanda-logo.png' plasma-org.kde.plasma.desktop-appletsrc
 
 # --- 7. REMOVING PROVISIONING CONFIG
